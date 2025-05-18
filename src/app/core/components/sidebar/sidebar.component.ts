6d741bbe04954e01f8edb127b2c30b1b6972b313
israelemanuel97 <israelemanuel97@gmail.com>
2025-05-18 11:48:00 -0300

    Atualização de inputs e de caminhos de telas

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import {MatRippleModule} from '@angular/material/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,8 +11,8 @@ import {MatRippleModule} from '@angular/material/core';
     CommonModule,
     MatIconModule,
     MatButtonModule,
-    MatRippleModule
-
+    MatRippleModule,
+    RouterLink,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
